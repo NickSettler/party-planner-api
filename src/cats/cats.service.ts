5b7204b63dd5d9df8a9bc41621f3690fdc163c31
NickSettler <moiseevnikita14@gmail.com>
2022-02-17 00:39:30 +0100

    moves cat entity

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Cat } from '../entities/cat.entity';
+import { Cat } from './cat.entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
