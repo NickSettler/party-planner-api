9e227e3544a5b34d8372968ebb2361eaf64024c7
NickSettler <moiseevnikita14@gmail.com>
2022-02-17 00:25:40 +0100

    updates cats controller

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -9,14 +9,17 @@ import {
   Res,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
+import { CatsService } from './cats.service';
 
 @Controller('cats')
 export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
   @Get()
   @HttpCode(200)
   @Header('Cache-Control', 'none')
-  public findAll(@Req() request: Request): string {
-    return `Get all cats. Method: ${request.method}`;
+  public async findAll(@Req() request: Request, @Res() response: Response) {
+    response.status(200).send(await this.catsService.findAll());
   }
 
   @Get('/:id')
