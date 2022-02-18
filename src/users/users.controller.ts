ab33d3eb2ab0472ed2819a36e7d26f4a262d4c3d
NickSettler <moiseevnikita14@gmail.com>
2022-02-18 22:37:37 +0100

    adds user creation route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,16 @@
-import { Controller, Get, NotFoundException, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Post,
+  UseGuards,
+  UsePipes,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { CreateUserDto } from './create-user.dto';
+import { ValidationPipe } from '../validation.pipe';
 
 @Controller('users')
 export class UsersController {
@@ -14,5 +24,12 @@ export class UsersController {
     if (!users) throw new NotFoundException();
 
     return users;
+  }
+
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  @UsePipes(ValidationPipe)
+  public async createUser(@Body() createUserDto: CreateUserDto) {
+    return await this.usersService.create(createUserDto);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
+import { CreateUserDto } from './create-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -15,5 +16,23 @@ export class UsersService {
 
   public findOne(username: string): Promise<User> {
     return this.userRepository.findOne({ username });
+  }
+
+  public async create(createDto: CreateUserDto): Promise<User> {
+    const { username, password } = createDto;
+
+    const userInDb = await this.userRepository.findOne({
+      username,
+    });
+
+    if (userInDb)
+      throw new HttpException('User already exists', HttpStatus.CONFLICT);
+
+    const user: User = await this.userRepository.create({
+      username,
+      password,
+    });
+    await this.userRepository.save(user);
+    return user;
   }
 }
