53e77829cae27e91e67c526c2ff7d3359d3d5419
NickSettler <moiseevnikita14@gmail.com>
2022-02-18 18:57:54 +0100

    adds config for auth, assets and views

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,38 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import { engine } from 'express-handlebars';
+import * as passport from 'passport';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'), {
+    prefix: '/public/',
+  });
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.engine(
+    'hbs',
+    engine({
+      extname: '.hbs',
+      defaultLayout: 'main',
+      partialsDir: [join(__dirname, '..', 'views', 'errors')],
+    }),
+  );
+  app.setViewEngine('hbs');
+
+  app.use(
+    session({
+      secret: 'secret',
+      resave: false,
+      saveUninitialized: true,
+    }),
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
+
   await app.listen(3000);
 }
 bootstrap();
