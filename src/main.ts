6f47e331cffe03bd0089dd1e74d5c1c0439e209f
NickSettler <moiseevnikita14@gmail.com>
2022-02-19 17:29:06 +0100

    removes other part of frontend

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
-import { engine } from 'express-handlebars';
-import * as passport from 'passport';
-import * as session from 'express-session';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
-  app.useStaticAssets(join(__dirname, '..', 'public'), {
-    prefix: '/public/',
-  });
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.engine(
-    'hbs',
-    engine({
-      extname: '.hbs',
-      defaultLayout: 'main',
-      partialsDir: [join(__dirname, '..', 'views', 'errors')],
-    }),
-  );
-  app.setViewEngine('hbs');
-
-  app.use(
-    session({
-      secret: 'secret',
-      resave: false,
-      saveUninitialized: true,
-    }),
-  );
-  app.use(passport.initialize());
-  app.use(passport.session());
 
   await app.listen(3000);
 }
