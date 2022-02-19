55e69834b097369cd544af56fcf815b9fc5bca00
NickSettler <moiseevnikita14@gmail.com>
2022-02-19 17:55:05 +0100

    adds api doc for cats

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -7,13 +7,27 @@ import {
 } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import {
+  ApiOkResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { Cat } from '../entities/cat.entity';
 
 @Controller('cats')
+@ApiTags('cats')
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Get()
   @UseGuards(JwtAuthGuard)
+  @ApiUnauthorizedResponse({
+    description: 'User is not authorized to access this route',
+  })
+  @ApiOkResponse({
+    description: 'List of cats',
+    type: [Cat],
+  })
   public async findAll() {
     const cats = await this.catsService.findAll();
 
--- src/entities/cat.entity.ts
+++ src/entities/cat.entity.ts
@@ -6,24 +6,29 @@ import {
   PrimaryGeneratedColumn,
 } from 'typeorm';
 import { User } from './user.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({
   name: 'cats',
 })
 export class Cat {
   @PrimaryGeneratedColumn()
+  @ApiProperty()
   id: number;
 
   @Column()
+  @ApiProperty()
   name: string;
 
   @Column({
     type: 'timestamp',
     default: () => 'CURRENT_TIMESTAMP',
   })
+  @ApiProperty()
   date_of_birth: Date;
 
   @ManyToOne(() => User, (user) => user.cats)
   @JoinColumn({ name: 'owner' })
+  @ApiProperty()
   owner: User;
 }
