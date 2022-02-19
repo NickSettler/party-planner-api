4e018ee858b5d54fb42e2705ddffaf9e4a8737f7
NickSettler <moiseevnikita14@gmail.com>
2022-02-19 17:56:26 +0100

    adds users table migration

---
+++ src/migrations/1645286724343-CreateUsersTable.ts
@@ -0,0 +1,53 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+import { Role } from '../roles/roles.enum';
+
+export class CreateUsersTable1645286724343 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            isNullable: false,
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+            isNullable: false,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+            onUpdate: 'now()',
+          },
+          {
+            name: 'roles',
+            type: 'enum',
+            enum: Object.values(Role),
+          },
+        ],
+      }),
+      true,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users', true, true, true);
+  }
+}
